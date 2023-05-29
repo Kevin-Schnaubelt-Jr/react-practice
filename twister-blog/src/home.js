2c76691ee32fb6053ef7e591fd8dd077fb27aec6
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-05-29 16:24:19 -0400

    update

--- twister-blog/src/home.js
+++ twister-blog/src/home.js
@@ -1,6 +1,19 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList"
 
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([    
+        { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+        { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+        { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 }
+    ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+
+    }
 
     // let name = "Kevin"
     const [name, setName] = useState("Kevin")
@@ -16,12 +29,19 @@ const Home = () => {
         console.log("Hello " + name, e.target)
     }
 
+    useEffect(() => {
+        console.log("use effect ran")
+        console.log(name)
+    }, [name])
+
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>Hello { name }! Wow, you're {age} years old...</p>
+            <h1>Hello { name }!</h1>
+            <p> Wow, you're {age} years old...</p>
             <button onClick={handleClick}>Click me</button>
             <button onClick={(e) => handleClickAgain(name, e)}>Click Me Again!</button>
+            <BlogList blogs={blogs} title="All Dem Blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's blogs" handleDelete={handleDelete}/>
         </div>
      );
 }
